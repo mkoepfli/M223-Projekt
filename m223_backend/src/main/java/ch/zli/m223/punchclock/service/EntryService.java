c7f0e58ae2832f608933644ac5d68be436ddeaf6
Marco <mkoepfli@hosttech.ch>
2021-09-09 14:18:34 +0200

    added Models

--- m223_backend/src/main/java/ch/zli/m223/punchclock/service/EntryService.java
+++ m223_backend/src/main/java/ch/zli/m223/punchclock/service/EntryService.java
@@ -1,5 +1,4 @@
 package ch.zli.m223.punchclock.service;
-
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -7,7 +6,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
-import ch.zli.m223.punchclock.domain.Entry;
+import ch.zli.m223.punchclock.domain.Entry.Entry;
 
 @ApplicationScoped
 public class EntryService {
